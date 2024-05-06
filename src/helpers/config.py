3e172ae07fb7b8ad167a085a2372437720b6acaa
khaled <khaledeemam@gmail.com>
2024-05-06 03:18:49 +0300

    ADD config for env variables to helpers

---
+++ src/helpers/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings , SettingsConfigDict
+      
+class Settings(BaseSettings) :
+
+    RAW_DATA_PATH : str
+    PRETRAINED_MODEL_NAME : str
+    PREPROCESSED_DATA_PATH : str
+    TAGS_COUNT :  int
+    MAX_LENGTH : int
+
+    class config:
+        env_file = ".env"
+
+
+def get_settings():
+    return Settings()
