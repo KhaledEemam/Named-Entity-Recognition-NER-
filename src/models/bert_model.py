6c33527f6e111d98e3705772e33a6897c001a57b
khaled <khaledeemam@gmail.com>
2024-05-06 05:30:11 +0300

    MODIFY model intialization

--- src/models/bert_model.py
+++ src/models/bert_model.py
@@ -1,12 +1,9 @@
 from transformers import BertForTokenClassification
 import os
-from dotenv import load_dotenv
-load_dotenv(r"E:\Khaled\Data\Projects\Named-Entity-Recognition-NER-\.env")
-PRETRAINED_MODEL_NAME = os.getenv("PRETRAINED_MODEL_NAME")
 
 class BertModel :
-    def __init__(self,num_labels) :
-        self.model = BertForTokenClassification.from_pretrained(PRETRAINED_MODEL_NAME,num_labels=num_labels)
+    def __init__(self,model_name,num_labels) :
+        self.model = BertForTokenClassification.from_pretrained(model_name,num_labels=num_labels)
 
 
     def get_model(self) :
