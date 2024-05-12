3826e53520bf27673fcb67cdae26ff5f6d42f977
khaled <khaledeemam@gmail.com>
2024-05-12 19:33:13 +0300

    ADD env variables to config.py

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -5,8 +5,11 @@ class Settings(BaseSettings) :
     RAW_DATA_PATH : str
     PRETRAINED_MODEL_NAME : str
     PREPROCESSED_DATA_PATH : str
+    SAVED_MODEL_PATH : str
+    SAVED_TOKENIZER_PATH : str
     TAGS_COUNT :  int
     MAX_LENGTH : int
+    EPOCHS : int
 
     class Config:
         env_file = ".env"
