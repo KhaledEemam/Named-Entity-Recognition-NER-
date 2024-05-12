4bdbaf24d5964917fe87e1cdd6ca36ca679ee47d
khaled <khaledeemam@gmail.com>
2024-05-12 19:13:16 +0300

    ADD train.py to tasks

---
+++ src/tasks/train.py
@@ -0,0 +1,37 @@
+from tqdm import tqdm
+import torch
+from sklearn.metrics import accuracy_score,precision_score, f1_score
+import numpy as np
+
+def train_model(model,optimizer,scheduler,device,data_loader) :
+  model.train()
+  losses = []
+  all_predictions = []
+  all_labels = []
+  for d in tqdm(data_loader) :
+      optimizer.zero_grad()
+      sequences = d["tokens"].to(device).to(torch.long)
+      targets = d["tags"].to(device).to(torch.long)
+      paddings = d["masks"].to(device).to(torch.long)
+
+      outputs = model(sequences, attention_mask=paddings, labels=targets)
+      ids = torch.argmax(outputs[1],axis=2)
+
+      flat_ids = ids.view(-1)
+      flat_targets = targets.view(-1)
+      masked_index = targets.view(-1) != -100
+      unmasked_preds = torch.masked_select(flat_ids,masked_index)
+      unmasked_tags = torch.masked_select(flat_targets,masked_index)
+      all_predictions.extend(unmasked_preds.cpu())
+      all_labels.extend(unmasked_tags.cpu())
+
+      loss = outputs[0]
+      losses.append(loss.item())
+      loss.backward()
+      optimizer.step()
+      scheduler.step()
+
+  accuracy = accuracy_score(all_predictions , all_labels)
+  f_score = f1_score(all_predictions , all_labels, average = 'weighted',zero_division=1)
+
+  return np.average(losses) , accuracy , f_score
